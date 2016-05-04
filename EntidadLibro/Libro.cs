d689dfe8d1d20603f2abc6b920c3064b79bf30b4
rojoherrero <rojoherrero@gmail.com>
2016-05-04 20:08:09 +0100

    Add project files.

---
+++ EntidadLibro/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadLibro
+{
+    public class Libro
+    {
+        public string Autor { get; set; }
+        public string Titulo { get; set; }
+        public string ISBN { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public int Ejemplares { get; set; }
+    }
+}
---
+++ GestionBiblioteca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadLibro;
+
+namespace GestionBiblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+                    
+
+        }
+    }
+}
